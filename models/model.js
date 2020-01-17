e636ac519d4a9f77a72e5f3510fd439e77fb1b02
BluDemons <jar.salazar@yavirac.edu.ec>
2020-01-17 11:36:49 -0500

    query con mysql

--- models/model.js
+++ models/model.js
@@ -2,13 +2,13 @@ const Sequelize = require('sequelize');
 
 const db = require('../db/dataBase');
 
-module.exports = db.sequelize.define('libro', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  nombre: Sequelize.STRING,
-  genero: Sequelize.STRING,
-  autor:  Sequelize.STRING,
+module.exports = db.sequelize.define('test', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nombre: Sequelize.STRING,
+    genero: Sequelize.STRING,
+    autor: Sequelize.STRING,
 });
--- ruta.js
+++ ruta.js
@@ -5,37 +5,38 @@ const router = express.Router();
 const Book = require('./models/model');
 
 router.get('/books', (req, res, next) => {
-  Book.findAll()
-    .then((books) => {
-      res.json(books);
-    })
-    .catch((err) => {
-      res.send(`error: ${err}`);
-    });
+    const { query } = req;
+    Book.findAll({ where: query })
+        .then((books) => {
+            res.json(books);
+        })
+        .catch((err) => {
+            res.send(`error: ${err}`);
+        });
 });
 
 router.post('/books', (req, res, next) => {
-  const datos = {
-    nombre: req.body.nombre,
-    genero: req.body.genero,
-    autor: req.body.autor
-  };
+    const datos = {
+        nombre: req.body.nombre,
+        genero: req.body.genero,
+        autor: req.body.autor
+    };
 
-  if (!datos) {
-    res.status(400);
-    res,
-    json({
-      error: 'Datos incorrectos'
-    });
-  } else {
-    Book.create(datos)
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => {
-        res.json(`error: ${err}`);
-      });
-  }
+    if (!datos) {
+        res.status(400);
+        res,
+        json({
+            error: 'Datos incorrectos'
+        });
+    } else {
+        Book.create(datos)
+            .then((data) => {
+                res.send(data);
+            })
+            .catch((err) => {
+                res.json(`error: ${err}`);
+            });
+    }
 });
 
 module.exports = router;
